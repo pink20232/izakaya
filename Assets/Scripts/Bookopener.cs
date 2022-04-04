7adfca194929a6e90ecaecae82f99bba59912c8e
pink20232 <pink20232@hotmail.com>
2022-04-04 10:56:14 -0700

    Books System

---
+++ Assets/Scripts/Bookopener.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Bookopener : MonoBehaviour
+{
+    
+    
+    public void ShowRecipe(GameObject recipe)
+    {
+        recipe.transform.DOScale(Vector3.zero, 1.0f).From().SetEase(Ease.OutElastic);
+    }
+
+}
