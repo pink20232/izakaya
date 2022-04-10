347f534a003246c5be8f16f308652a08b2d98350
pink20232 <pink20232@hotmail.com>
2022-04-09 22:11:15 -0700

    XR Socket interactable, New Character, Book System

---
+++ Assets/Scripts/PlateSocketInteractor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit; 
+
+
+public class PlateSocketInteractor : XRSocketInteractor
+{
+    
+    public string TagtoCompare = "sushi";
+    // Start is called before the first frame update
+    
+}
