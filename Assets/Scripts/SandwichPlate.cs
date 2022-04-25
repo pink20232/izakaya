2a8b326a3e837ce87195d0793f17ec8c1684e766
pink20232 <pink20232@hotmail.com>
2022-04-25 15:18:16 -0700

    Add Graphics and Make Sandwich
    Add Graphics and Make Sandwich

---
+++ Assets/Scripts/SandwichPlate.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SandwichPlate : MonoBehaviour
+{
+    public GameObject sandwichPiece1, sandwichPiece2, sandwichPiece3;
+    public GameObject assembleSandwich;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.name == "Sandwich_p1")
+        {
+            sandwichPiece1.SetActive(true);
+            Destroy(other.gameObject);
+        }else if (other.gameObject.name == "Sandwich_p2")
+        {
+            sandwichPiece2.SetActive(true);
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.name == "Sandwich_p2")
+        {
+            sandwichPiece2.SetActive(true);
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.name == "Sandwich_p3")
+        {
+            sandwichPiece3.SetActive(true);
+            Destroy(other.gameObject);
+        }
+
+
+
+        if (sandwichPiece1.activeSelf &&  sandwichPiece2.activeSelf && sandwichPiece3.activeSelf)
+        {
+            assembleSandwich.GetComponent<XRGrabInteractable>().enabled = true;
+            assembleSandwich.GetComponent<Rigidbody>().isKinematic = false;
+            //from preset kinematic to use gravity 
+            assembleSandwich.GetComponent<Rigidbody>().useGravity = true;
+            //indika's example: workbench which to hold all the objects 
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UIOrientation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOrientation : MonoBehaviour
+{
+    private Transform XRCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRCamera = Camera.main.transform;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(XRCamera);  
+    }
+}
