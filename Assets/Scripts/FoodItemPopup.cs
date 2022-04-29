fdd792bff6ab0dcd9bea78b29ab3669f2013ec30
pink20232 <pink20232@hotmail.com>
2022-04-28 21:25:50 -0700

    book system logic completed
    book system logic completed

---
+++ Assets/Scripts/FoodItemPopup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class FoodItemPopup : MonoBehaviour
+{
+    public float delay = 0.5f;
+    void OnEnable()
+    {
+        transform.DOScale(Vector3.zero, 1.0f).From().SetEase(Ease.OutElastic).SetDelay(delay);
+    }
+}
